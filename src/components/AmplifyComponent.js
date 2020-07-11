a202c58b23e0892c7cbbeb3369bda9cb5eb3cf3c
jeffy-zhu <zjeff.jz@gmail.com>
2020-07-11 00:52:27 -0400

    Made home page responsive to window resizing

--- src/components/AmplifyComponent.js
+++ src/components/AmplifyComponent.js
@@ -9,22 +9,29 @@ class AmplifyComponent extends React.Component {
 
   render() {
     return (
-      <div id="centered">
-        <img id="guitar" src={guitar} alt="" />
-        <h1>Welcome to Amplify!</h1>
-        <div id="instructions">Enter the name of an artist or song</div>
-        <input
-          onChange={(e) => this.setState({ searchItem: e.target.value })}
-        />
-        <button
-          id="search-button"
-          onClick={() =>
-            this.props.history.push(`/search/${this.state.searchItem}`)
-          }
-          disabled={!this.state.searchItem}
-        >
-          Search
-        </button>
+      <div className="row">
+        <div id="centered" className="col">
+          <div id="guitar-welcome-parent">
+            <img id="guitar" src={guitar} alt="" />
+            <div>
+              <h1 id="welcome">Welcome to Amplify!</h1>
+              <div id="instructions">Enter the name of an artist or song</div>
+              <input
+                id="search-input"
+                onChange={(e) => this.setState({ searchItem: e.target.value })}
+              />
+              <button
+                id="search-button"
+                onClick={() =>
+                  this.props.history.push(`/search/${this.state.searchItem}`)
+                }
+                disabled={!this.state.searchItem}
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
